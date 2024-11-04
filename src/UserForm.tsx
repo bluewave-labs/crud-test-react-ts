adc40d251fdb3aef631548c8963063e04cc67694
salmanja <71104867+salmanja@users.noreply.github.com>
2024-11-04 13:58:46 -0500

    added validation to the data form and debugged phone validation library

--- src/UserForm.tsx
+++ src/UserForm.tsx
@@ -1,49 +1,119 @@
-import React, {useState,useEffect} from 'react';
-import {User} from './types';
+import React, { useState, useEffect } from "react";
+import { User } from "./types";
+import { isValidPhoneNumber } from "libphonenumber-js";
 
 interface UserFormProps {
-    onSave:(user:User)=>void;
-    editingUser: User | null;
-    onEditSave:(user:User) =>void;
+  onSave: (user: User) => void;
+  editingUser: User | null;
+  onEditSave: (user: User) => void;
 }
-const UserForm: React.FC<UserFormProps> = ({onSave, editingUser,onEditSave})=>{
-    const [formData,setFormData] = useState<User>({id: '',firstName: '', lastName: '',emailAddress:'',phone:'', dateOfBirth:''});
+const UserForm: React.FC<UserFormProps> = ({
+  onSave,
+  editingUser,
+  onEditSave,
+}) => {
+  const [formData, setFormData] = useState<User>({
+    id: "",
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+    phone: "",
+    dateOfBirth: "",
+  });
+  const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
-    useEffect(()=>{
-        if (editingUser) setFormData(editingUser);
-    }, [editingUser]);
+  useEffect(() => {
+    if (editingUser) setFormData(editingUser);
+  }, [editingUser]);
 
-    const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
-        const {name,value} = e.target;
-        setFormData(prev =>({...prev,[name]:value}));
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
 
+  //validating form inputs
+  const validateFields = () => {
+    let valid = true;
+    const newErrors: { [key: string]: string } = {};
+
+    if (!formData.firstName) {
+      newErrors.firstName = "first name required";
+      valid = false;
     }
-    const handleSubmit = (e:React.FormEvent) =>{
-        e.preventDefault();
-        if(editingUser){
-            onEditSave(formData);
-        }else{
-            onSave({...formData, id:Date.now().toString()});
-        }
-        setFormData({
-          id: "",
-          firstName: "",
-          lastName: "",
-          emailAddress: "",
-          phone: "",
-          dateOfBirth: "",
-        });
+    if (!formData.lastName) {
+      newErrors.lastName = "last name required";
+      valid = false;
     }
-    return (
-      <form onSubmit={handleSubmit}>
-        <input name='firstName' value={formData.firstName} onChange={handleChange} placeholder='first name' />
-        <input name='lastName' value={formData.lastName} onChange={handleChange} placeholder='last name'/>
-        <input name='emailAddress' value={formData.emailAddress} onChange={handleChange} placeholder='email address' />
-        <input name='phone' value={formData.phone} onChange={handleChange} placeholder='phone'/>
-        <input name='dateOfBirth' value={formData.dateOfBirth} onChange={handleChange} placeholder='date of birth'/>
-        <button type='submit'>{editingUser ? 'save changes': 'add user'}</button>
-      </form>
-    );
-}
+    if (!formData.emailAddress || !/^%&#*!+>+$/.test(formData.emailAddress)) {
+      newErrors.emailAddress = "valid email required";
+      valid = false;
+    }
+    if (!formData.phone || !isValidPhoneNumber(formData.phone)) {
+      newErrors.phone = "valid phone required";
+      valid = false;
+    }
+    setErrors(newErrors);
+    return valid;
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(!validateFields()) return;
+
+    if (editingUser) {
+      onEditSave(formData);
+    } else {
+      onSave({ ...formData, id: Date.now().toString() });
+    }
+    setFormData({
+      id: "",
+      firstName: "",
+      lastName: "",
+      emailAddress: "",
+      phone: "",
+      dateOfBirth: "",
+    });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="firstName"
+        value={formData.firstName}
+        onChange={handleChange}
+        placeholder="first name"
+      />
+      {errors.firstName && <span>{errors.firstName}</span>}
+      <input
+        name="lastName"
+        value={formData.lastName}
+        onChange={handleChange}
+        placeholder="last name"
+      />
+      {errors.lastName && <span>{errors.lastName}</span>}
+      <input
+        name="emailAddress"
+        value={formData.emailAddress}
+        onChange={handleChange}
+        placeholder="email address"
+      />
+      {errors.emailAddress && <span>{errors.emailAddress}</span>}
+      <input
+        name="phone"
+        value={formData.phone}
+        onChange={handleChange}
+        placeholder="phone"
+      />
+      {errors.phone && <span>{errors.phone}</span>}
+      <input
+        name="dateOfBirth"
+        value={formData.dateOfBirth}
+        onChange={handleChange}
+        placeholder="date of birth"
+      />
+      <button type="submit">{editingUser ? "save changes" : "add user"}</button>
+    </form>
+  );
+};
 
 export default UserForm;
